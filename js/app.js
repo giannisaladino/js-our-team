031d510591d8af73a30ed82dd544153ddd00b74e
giannisaladino <saladinogianni6@gmail.com>
2024-03-18 15:09:07 +0100

    ADD: array of object

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//Creare un array di oggetti per rappresentare i membri del team
+
+// 1 - creo l'array di oggetti
+const teamMember = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg',
+    }, 
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg',
+    }, 
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg',
+    }, 
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg',
+    }, 
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg',
+    }, 
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg',
+    }, 
+]
+
+console.log(teamMember);
+
+// 2 - stampo su console per ogni membro il nome, ruolo e la stringa della foto
+// 3 - stampo tutto sul dom
